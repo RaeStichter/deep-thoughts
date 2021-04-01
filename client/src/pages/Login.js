b6cd132685a68bece8d04e4a4c6d06cffeec7632
Rae Stichter <raestichter@gmail.com>
2021-04-01 02:47:05 -0400

    updated the user experience and added authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,15 +18,29 @@ const Login = (props) => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
-
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+  
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+  
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
   };
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   // clear form values
+  //   setFormState({
+  //     email: '',
+  //     password: '',
+  //   });
+  // };
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +71,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
